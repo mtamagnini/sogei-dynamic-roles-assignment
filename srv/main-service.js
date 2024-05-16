893efb74cfb08bfde39461f63b9ba3dae563fc5a
Andrea Furlan <andrea.furlan@sap.com>
2024-05-16 09:15:49 +0000

    Add auth

--- srv/main-service.js
+++ srv/main-service.js
@@ -1,8 +1,17 @@
 module.exports = async (srv) => {
 
+    const wzService = await cds.connect.to('workzone-api');
+
     srv.on("updateUsersLists", async (req) => {
         const { userId, mainRole, subRole } = req.data;
-        //req.user
+        //const user = req.user;
+
+
+        var aUsers = await wzService.send({
+            method: 'GET',
+            path: `/api/v1/scim/Users`
+        });
+
         return {
             status: "succeed",
             message: "Change Done!"
